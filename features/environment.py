7b3248b3a0d08cee9187f32be365ed54310806d3
Francis <flin0002@student.monash.edu>
2020-10-08 10:29:42 +1100

    fix error

--- features/environment.py
+++ features/environment.py
@@ -11,9 +11,12 @@ def before_all(context):
 def before_feature(context, feature):
 
     if 'iOS' in feature.tags:
+        app = os.path.join(os.path.dirname(__file__), '../apps/ios/UIKitCatalog', 'UIKitCatalog-iphonesimulator.zip')
+        app = os.path.abspath(app)
         context.driver = webdriver.Remote(
             command_executor='http://127.0.0.1:4723/wd/hub',
             desired_capabilities={
+                'app': app,
                 'platformName': 'iOS',
                 'deviceName': 'iPhone 11',
                 'platformVersion': '13.7',

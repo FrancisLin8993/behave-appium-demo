5fa7d3205c91142318c063551b2fd6959bde55b3
Francis <flin0002@student.monash.edu>
2020-10-08 11:18:15 +1100

    fix error of ios test

--- features/environment.py
+++ features/environment.py
@@ -24,6 +24,24 @@ def before_feature(context, feature):
                 'noReset': 'true'
             })
 
+    elif 'android' in feature.tags:
+        app = os.path.join(os.path.dirname(__file__),
+                           '../apps/',
+                           'ApiDemos-debug.apk')
+        app = os.path.abspath(app)
+        context.driver = webdriver.Remote(
+            command_executor='http://127.0.0.1:4723/wd/hub',
+            desired_capabilities={
+                'app': app,
+                'platformName': 'Android',
+                'platformVersion': '11.0',
+                'deviceName': 'emulator-5554',
+                'appActivity': '.ApiDemos',
+                'appPackage': 'io.appium.android.apis',
+                'automationName': 'UiAutomator2',
+                'uiautomator2ServerInstallTimeout': '60000'
+            })
+
 
 
 def after_feature(context, feature):

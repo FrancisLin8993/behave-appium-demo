1b8647998ca3081de3bfe56de62d56ecc1045356
Francis <flin0002@student.monash.edu>
2020-10-09 16:41:29 +1100

    add jira integration

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,16 @@
 import os
+import logging
+import datetime
+import shutil
 from time import sleep
-
 from appium import webdriver
-
+from allure_commons._allure import attach
+from allure_commons.types import AttachmentType
+from utilities.jira import jira
+from behave.model_core import Status
 
 def before_all(context):
-    context.config.setup_logging()
+    jira.connect_to_jira()
     pass
 
 def before_feature(context, feature):
@@ -48,3 +53,48 @@ def after_feature(context, feature):
     sleep(1)
     context.driver.save_screenshot("features/reports/screen_final.png")
     context.driver.quit()
+
+
+def before_scenario(context, scenario):
+    logging.info("START scenario: " + scenario.name)
+
+def after_step(context, step):
+
+    if step.status == "failed":
+        attach(context.driver.get_screenshot_as_png(), name=datetime.datetime.now().timestamp(), attachment_type = AttachmentType.PNG)
+        # jira.attach_screenshots_in_jira(context.driver.capture_screenshots_for_jira())
+
+
+def after_scenario(context, scenario):
+    logging.info(scenario.status)
+    if scenario.status == Status.failed:
+
+        scenario_name = "Scenario Name: " + scenario.name
+        description = ""
+        for step in scenario.steps:
+            rows = ""
+            description += str(step) + "\n"
+            if step.table is not None:
+                rows = '\n'.join(str(row) for row in step.table.rows)
+                rows = rows.replace("Row [", "")
+                rows = rows.replace("]", "")
+                description += str(rows) + "\n"
+        description = description.replace("<", "")
+        description = description.replace(">", "")
+
+        jira.add_issue(scenario_name, description)
+
+    logging.info("FINISHED scenario: " + scenario.name)
+
+
+
+
+
+def capture_screenshots_for_jira(context):
+    path = os.getcwd()+"/temp"
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.mkdir(path)
+    path_to_capture_screenshot = path+"/"+str(datetime.datetime.now().timestamp())+".png"
+    context.driver.get_screenshot_as_file(path_to_capture_screenshot)
+    return path_to_capture_screenshot
